7dd4f40f5784e5af560c5fbb0a251cda30fdd61b
Santiago Squarzon <squarzon.santiago@gmail.com>
2022-05-27 16:32:41 -0300

    .

--- script1.ps1
+++
@@ -1,9 +0,0 @@
-$output = "@description('Name of the resource')
-output resourceName array = [for (item, i) in databases: db[i].name]
-@description('ID of the resource')
-output resourceID array = [for (item, i) in databases: db[i].id]"
-
-$value = "[for (item, i) in databases: db[i].id]"
-
-$value = [Regex]::Escape($value)
-$output -match "(?<==\s)$value(?=`"|$)"
