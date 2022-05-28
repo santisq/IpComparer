d9233fed7e44c11f10599ac8001f3d0d609877fe
Santiago Squarzon <squarzon.santiago@gmail.com>
2022-05-28 01:11:06 -0300

    .

--- script3.ps1
+++ script3.ps1
@@ -1,18 +1,1 @@
-function CompareTo ([ipaddress] $Ip1, [ipaddress] $Ip2) {
-    $lhs = $Ip1.GetAddressBytes()
-    $rhs = $Ip2.GetAddressBytes()
-
-    for($i = 0; $i -lt 4; $i++) {
-        if($lhs[$i] -eq $rhs[$i]) {
-            continue
-        }
-        if($lhs[$i] -lt $rhs[$i]) {
-            return -1
-        }
-
-        return 1
-    }
-}
-
-CompareTo '85.185.240.128' '195.146.40.0'
-CompareTo '195.146.40.0' '85.185.240.128'
+1 -gt (Get-Process)
